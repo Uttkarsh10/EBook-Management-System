1b04807989455e64dd5f5f15783c5ca0a8777b12
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-02 23:34:43 +0530

    Update BookDAOImpl.java

--- EBook-App/src/main/java/com/DAO/BookDAOImpl.java
+++ EBook-App/src/main/java/com/DAO/BookDAOImpl.java
@@ -224,4 +224,93 @@ public class BookDAOImpl implements BookDAO {
 		return list;
 	}
 
+	public List<BookDtls> getAllRecentBook() {
+		List<BookDtls> list = new ArrayList<BookDtls>();
+		BookDtls b  = null;
+		try {
+			String sql = "select * from book_dtls where status=? order by bookId DESC";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, "Active");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				b=new BookDtls();
+				b.setBookId(rs.getInt(1));
+				b.setBookName(rs.getString(2));
+				b.setAuthor(rs.getString(3));
+				b.setPrice(rs.getString(4));
+				b.setBookCategory(rs.getString(5));
+				b.setStatus(rs.getString(6));
+				b.setPhotoName(rs.getString(7));
+				b.setEmail(rs.getString(8));
+				list.add(b);
+			}
+			
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
+	public List<BookDtls> getAllOldBook() {
+		List<BookDtls> list = new ArrayList<BookDtls>();
+		BookDtls b  = null;
+		try {
+			String sql = "select * from book_dtls where bookCategory=? and status=? order by bookId DESC";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, "Old");
+			ps.setString(2, "Active");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				b=new BookDtls();
+				b.setBookId(rs.getInt(1));
+				b.setBookName(rs.getString(2));
+				b.setAuthor(rs.getString(3));
+				b.setPrice(rs.getString(4));
+				b.setBookCategory(rs.getString(5));
+				b.setStatus(rs.getString(6));
+				b.setPhotoName(rs.getString(7));
+				b.setEmail(rs.getString(8));
+				list.add(b);
+			}
+			
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
+	public List<BookDtls> getAllNewBook() {
+		List<BookDtls> list = new ArrayList<BookDtls>();
+		BookDtls b  = null;
+		try {
+			String sql = "select * from book_dtls where bookCategory=? and status=? order by bookId DESC";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, "New");
+			ps.setString(2, "Active");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				b=new BookDtls();
+				b.setBookId(rs.getInt(1));
+				b.setBookName(rs.getString(2));
+				b.setAuthor(rs.getString(3));
+				b.setPrice(rs.getString(4));
+				b.setBookCategory(rs.getString(5));
+				b.setStatus(rs.getString(6));
+				b.setPhotoName(rs.getString(7));
+				b.setEmail(rs.getString(8));
+				list.add(b);
+			}
+			
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
 }
