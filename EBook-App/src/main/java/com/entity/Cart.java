d2ed44776f3e2ad1ea3674ebed2680d0f91dc846
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-04 01:47:41 +0530

    Add files via upload

---
+++ EBook-App/src/main/java/com/entity/Cart.java
@@ -0,0 +1,54 @@
+package com.entity;
+
+public class Cart {
+	private int cid;
+	private int bid;
+	private int userid;
+	private String bookName;
+	private String author;
+	private Double price;
+	private Double totalPrice;
+	
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+}
