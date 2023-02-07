d4dab256e27e03e159d9824ece4c52e8e4a66cd3
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-07 23:42:58 +0530

    Update CartDAOImpl.java

--- EBook-App/src/main/java/com/DAO/CartDAOImpl.java
+++ EBook-App/src/main/java/com/DAO/CartDAOImpl.java
@@ -2,7 +2,11 @@ package com.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.entity.BookDtls;
 import com.entity.Cart;
 import com.mysql.cj.xdevapi.PreparableStatement;
 
@@ -32,6 +36,51 @@ public class CartDAOImpl implements CartDAO {
 			e.printStackTrace();
 		}
 		
+		return f;
+	}
+
+	public List<Cart> getBookByUser(int userId) {
+		List<Cart> list = new ArrayList<Cart>();
+		Cart c = null;
+		double totalPrice=0;
+		try {
+			String sql = "select * from cart where uid=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1,userId);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				c = new Cart();
+				c.setCid(rs.getInt(1));
+				c.setBid(rs.getInt(2));
+				c.setUserid(rs.getInt(3));
+				c.setBookName(rs.getString(4));
+				c.setAuthor(rs.getString(5));
+				c.setPrice(rs.getDouble(6));
+				
+				totalPrice=totalPrice+rs.getDouble(7);
+				c.setTotalPrice(totalPrice);
+				list.add(c);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public boolean deleteBook(int bid,int uid) {
+		boolean f=false;
+		try {
+			String sql = "delete from cart where bid=? and uid=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1,bid);
+			ps.setInt(2,uid);
+			int i=ps.executeUpdate();
+			if(i==1) f=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return f;
 	}
 
