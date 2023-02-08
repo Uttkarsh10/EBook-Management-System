471a54d48036a94cc1a801dc9a41720940208469
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-08 23:05:31 +0530

    Update CartDAO.java

--- EBook-App/src/main/java/com/DAO/CartDAO.java
+++ EBook-App/src/main/java/com/DAO/CartDAO.java
@@ -7,5 +7,5 @@ import com.entity.Cart;
 public interface CartDAO {
 	public boolean addCart(Cart c);
 	public List<Cart> getBookByUser(int userId);
-	public boolean deleteBook(int bid,int uid);
+	public boolean deleteBook(int bid,int uid,int cid);
 }
