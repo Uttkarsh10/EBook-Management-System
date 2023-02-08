28e7483bff91d62938c91176ebb01cb9db75eeb4
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-08 23:04:43 +0530

    Update BookDAOImpl.java

--- EBook-App/src/main/java/com/DAO/BookDAOImpl.java
+++ EBook-App/src/main/java/com/DAO/BookDAOImpl.java
@@ -313,4 +313,50 @@ public class BookDAOImpl implements BookDAO {
 		return list;
 	}
 
+	public List<BookDtls> getBookByOld(String email, String cate) {
+		List<BookDtls> list = new ArrayList<BookDtls>();
+		BookDtls b  = null;
+		try {
+			String sql = "select * from book_dtls where bookCategory=? and email=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, cate);
+			ps.setString(2, email);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				b=new BookDtls();
+				b.setBookId(rs.getInt(1));
+				b.setBookName(rs.getString(2));
+				b.setAuthor(rs.getString(3));
+				b.setPrice(rs.getString(4));
+				b.setBookCategory(rs.getString(5));
+				b.setStatus(rs.getString(6));
+				b.setPhotoName(rs.getString(7));
+				b.setEmail(rs.getString(8));
+				list.add(b);
+			}
+			
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
+	public boolean oldBookDelete(String email, String cate, int id) {
+		boolean f=false;
+		try {
+			String sql = "delete from book_dtls where bookCategory=? and email=? and bookId=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, cate);
+			ps.setString(2, email);
+			ps.setInt(3,id);
+			int i=ps.executeUpdate();
+			if(i==1) f=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return f;
+	}
+
 }
