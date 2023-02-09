ac32841317c8addab91397003e223ceb1cfa8e46
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-09 15:12:26 +0530

    Update UserDAO.java

--- EBook-App/src/main/java/com/DAO/UserDAO.java
+++ EBook-App/src/main/java/com/DAO/UserDAO.java
@@ -4,6 +4,7 @@ import com.entity.user;
 
 public interface UserDAO {
 	public boolean userRegister(user us);
-	
 	public user login(String email, String password);
+	public boolean checkPassword(int id, String ps);
+	public boolean updateProfile(user us);
 }
