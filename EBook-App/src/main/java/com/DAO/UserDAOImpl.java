d6cebc1bf98305ac28964799cc8f235f10f93845
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-09 15:12:56 +0530

    Update UserDAOImpl.java

--- EBook-App/src/main/java/com/DAO/UserDAOImpl.java
+++ EBook-App/src/main/java/com/DAO/UserDAOImpl.java
@@ -65,5 +65,46 @@ public class UserDAOImpl implements UserDAO{
 		}
 		return us;
 	}
+
+
+	public boolean checkPassword(int id, String ps) {
+		boolean f=false;
+		try {
+			String sql = "select * from user where id=? and password=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			pst.setString(2, ps);
+			
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				f=true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return f;
+	}
+
+
+	public boolean updateProfile(user us) {
+		boolean f = false;
+		try {
+			String sql = "update user set name=?,email=?,phno=? where id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1,us.getName());
+			ps.setString(2,us.getEmail());
+			ps.setString(3,us.getPhno());
+			ps.setInt(4,us.getId());
+			
+			int i = ps.executeUpdate();
+			if(i==1) f=true;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return f;
+	}
 	
 }
