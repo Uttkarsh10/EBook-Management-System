686b76391dd2a3eef7db4db2732731091c8f3a54
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-11 23:41:18 +0530

    Update BookOrderDAO.java

--- EBook-App/src/main/java/com/DAO/BookOrderDAO.java
+++ EBook-App/src/main/java/com/DAO/BookOrderDAO.java
@@ -7,4 +7,7 @@ import com.entity.Book_Order;
 public interface BookOrderDAO {
 	//public int getOrderNo();
 	public boolean saveOrder(List<Book_Order> b);
+	public List<Book_Order> getBook(String email);
+	
+	public List<Book_Order> getAllOrder();
 }
