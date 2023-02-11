9761e49be03244dbb99edfd26b2d0dc2e2a33fd5
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-11 23:41:45 +0530

    Update BookOrderImpl.java

--- EBook-App/src/main/java/com/DAO/BookOrderImpl.java
+++ EBook-App/src/main/java/com/DAO/BookOrderImpl.java
@@ -1,8 +1,10 @@
 package com.DAO;
 
+import java.awt.print.Book;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.entity.Book_Order;
@@ -47,6 +49,69 @@ public class BookOrderImpl implements BookOrderDAO{
 		}
 		
 		return f;
+	}
+
+	public List<Book_Order> getBook(String email) {
+		List<Book_Order> list = new ArrayList<Book_Order>();
+		Book_Order o =  null;
+		
+		try {
+			String sql = "Select * from book_order where email=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1,email);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				o = new Book_Order();
+				o.setId(rs.getInt(1));
+				o.setOrderId(rs.getString(2));
+				o.setUsername(rs.getString(3));
+				o.setEmail(rs.getString(4));
+				o.setFulladd(rs.getString(5));
+				o.setPhno(rs.getString(6));
+				o.setBookName(rs.getString(7));
+				o.setAuthor(rs.getString(8));
+				o.setPrice(rs.getString(9));
+				o.setPaymentType(rs.getString(10));
+				list.add(o);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+	}
+
+	public List<Book_Order> getAllOrder() {
+		List<Book_Order> list = new ArrayList<Book_Order>();
+		Book_Order o =  null;
+		
+		try {
+			String sql = "Select * from book_order";
+			PreparedStatement ps = conn.prepareStatement(sql); 
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				o = new Book_Order();
+				o.setId(rs.getInt(1));
+				o.setOrderId(rs.getString(2));
+				o.setUsername(rs.getString(3));
+				o.setEmail(rs.getString(4));
+				o.setFulladd(rs.getString(5));
+				o.setPhno(rs.getString(6));
+				o.setBookName(rs.getString(7));
+				o.setAuthor(rs.getString(8));
+				o.setPrice(rs.getString(9));
+				o.setPaymentType(rs.getString(10));
+				list.add(o);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return list;
 	}
 
 }
