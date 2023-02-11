cb80895531969c96b1ac1467d7005d34775632ff
Uttkarsh Kumar Saini <uttkarshsaini10@gmail.com>
2023-02-11 23:42:21 +0530

    Update OrderServlet.java

--- EBook-App/src/main/java/com/user/servlet/OrderServlet.java
+++ EBook-App/src/main/java/com/user/servlet/OrderServlet.java
@@ -21,12 +21,12 @@ import com.entity.Cart;
 import com.mysql.cj.Session;
 
 @WebServlet("/order")
-public class OrderServlet extends HttpServlet{
+public class OrderServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
-			
+
 			HttpSession session = req.getSession();
 			int id = Integer.parseInt(req.getParameter("id"));
 			String name = req.getParameter("username");
@@ -36,58 +36,61 @@ public class OrderServlet extends HttpServlet{
 			String landmark = req.getParameter("landmark");
 			String city = req.getParameter("city");
 			String state = req.getParameter("state");
-			String  pincode = req.getParameter("pincode");
+			String pincode = req.getParameter("pincode");
 			String paymentType = req.getParameter("payment");
-			
-			String fullAdd = address+", "+landmark+", "+city+", "+state+", "+pincode;
-			
-			//System.out.println(name+" "+email+" "+phno+" "+paymentType);
-			
+
+			String fullAdd = address + ", " + landmark + ", " + city + ", " + state + ", " + pincode;
+
+			// System.out.println(name+" "+email+" "+phno+" "+paymentType);
+
 			CartDAOImpl dao = new CartDAOImpl(DBConnect.getconn());
-			
 			List<Cart> blist = dao.getBookByUser(id);
-			BookOrderImpl dao2 = new BookOrderImpl(DBConnect.getconn());
-			Book_Order o = null;
-			
-			ArrayList<Book_Order> orderList = new ArrayList<Book_Order>();
-			Random r = new Random();
-			for(Cart c : blist) {
-				//System.out.println(c.getBookName()+" "+c.getAuthor()+" "+c.getAuthor());
-				o = new Book_Order();
-				o.setOrderId("BOOK-ORD-00" + r.nextInt(1000));
-				o.setUsername(name);
-				o.setEmail(email);
-				o.setPhno(phno);
-				o.setFulladd(fullAdd);
-				o.setBookName(c.getBookName());
-				o.setAuthor(c.getAuthor());
-				o.setPrice(c.getPrice()+"");
-				o.setPaymentType(paymentType);
-				
-				orderList.add(o);
-			}
-			
-			
-			if("noselect".equals(paymentType)) {
-				session.setAttribute("failedMsg","Choose Payment Method to proceed");
+			if (blist.isEmpty()) {
+				session.setAttribute("failedMsg", "Add Item");
 				resp.sendRedirect("checkout.jsp");
-			}
-			else {
-				boolean f = dao2.saveOrder(orderList);
-				
-				if(f) {
-					resp.sendRedirect("order_success.jsp");
+			} else {
+
+				BookOrderImpl dao2 = new BookOrderImpl(DBConnect.getconn());
+				Book_Order o = null;
+
+				ArrayList<Book_Order> orderList = new ArrayList<Book_Order>();
+				Random r = new Random();
+				for (Cart c : blist) {
+					// System.out.println(c.getBookName()+" "+c.getAuthor()+" "+c.getAuthor());
+					o = new Book_Order();
+					o.setOrderId("BOOK-ORD-00" + r.nextInt(1000));
+					o.setUsername(name);
+					o.setEmail(email);
+					o.setPhno(phno);
+					o.setFulladd(fullAdd);
+					o.setBookName(c.getBookName());
+					o.setAuthor(c.getAuthor());
+					o.setPrice(c.getPrice() + "");
+					o.setPaymentType(paymentType);
+
+					orderList.add(o);
 				}
-				
-				else {
-					session.setAttribute("failedMsg","Your Order failed");
+
+				if ("noselect".equals(paymentType)) {
+					session.setAttribute("failedMsg", "Choose Payment Method to proceed");
 					resp.sendRedirect("checkout.jsp");
+				} else {
+					boolean f = dao2.saveOrder(orderList);
+
+					if (f) {
+						resp.sendRedirect("order_success.jsp");
+					}
+
+					else {
+						session.setAttribute("failedMsg", "Your Order failed");
+						resp.sendRedirect("checkout.jsp");
+					}
 				}
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
